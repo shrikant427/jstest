994dbd0dbf8d7cddc472ea11b81f25700df85051
Atul Kumar <atulkumar@Atuls-MacBook-Air.local>
2024-03-18 20:45:36 +0530

    update button

--- contact-form-submit.js
+++ contact-form-submit.js
@@ -4,6 +4,11 @@ document.getElementById("contact-no").addEventListener("input", function(event) 
 });
 
 function submitForm() {
+
+    var submitButton = document.getElementById("submit-button");
+    submitButton.disabled = true;
+    submitButton.textContent = "Processing...";
+
     var formData = {
         name: document.getElementById("your-name").value,
         email: document.getElementById("your-email").value,
@@ -12,7 +17,7 @@ function submitForm() {
     };
 
     var xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://www.aksystem.in:8443/contact/racekon", true);
+    xhr.open("POST", "https://aksystem.in:8443/contact/racekon", true);
     xhr.setRequestHeader("Content-Type", "application/json");
     xhr.onreadystatechange = function () {
         if (xhr.readyState === XMLHttpRequest.DONE) {
@@ -23,7 +28,10 @@ function submitForm() {
             } else {
                 document.getElementById("form-message").innerText = "Failed to send message.";
             }
+            submitButton.textContent = "SEND MESSAGE";
+            submitButton.disabled = false;
         }
     };
+    xhr.withCredentials = false;
     xhr.send(JSON.stringify(formData)); // Send form data as JSON
 }
