f732dbfbe91b4a51675bea96d9690357d5ffa890
Atul Kumar <atulkumar@Atuls-MacBook-Air.local>
2024-03-18 19:49:11 +0530

    update contact form

---
+++ contact-form-submit.js
@@ -0,0 +1,29 @@
+// Function to allow only numeric input for the phone number field
+document.getElementById("contact-no").addEventListener("input", function(event) {
+    this.value = this.value.replace(/[^0-9-,+]/g, ""); // Remove non-numeric characters
+});
+
+function submitForm() {
+    var formData = {
+        name: document.getElementById("your-name").value,
+        email: document.getElementById("your-email").value,
+        subject: document.getElementById("purpose-of-contact").value,
+        message: document.getElementById("message").value + ' \nmobile - '+ document.getElementById("contact-no").value
+    };
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", "https://www.aksystem.in:8443/contact/racekon", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            if (xhr.status === 200) {
+                document.getElementById("form-message").innerText = "Message sent successfully!";
+                // Optionally reset the form
+                document.getElementById("contact-form").reset();
+            } else {
+                document.getElementById("form-message").innerText = "Failed to send message.";
+            }
+        }
+    };
+    xhr.send(JSON.stringify(formData)); // Send form data as JSON
+}
